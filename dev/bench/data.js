897d244b069d7c22fa24943dd7d36d2e8cc8d32a
github-action-benchmark <github@users.noreply.github.com>
2026-09-11 10:51:49 +0000

    add Benchmark (cargo) benchmark result for 535ddeb64f4f124435919d2f568fce17d17aad9e

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1789123909450,
+  "repoUrl": "https://github.com/dasantos/ferx",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "danielsantos5589@gmail.com",
+            "name": "Daniel Santos",
+            "username": "dasantos"
+          },
+          "committer": {
+            "email": "danielsantos5589@gmail.com",
+            "name": "Daniel Santos",
+            "username": "dasantos"
+          },
+          "distinct": true,
+          "id": "535ddeb64f4f124435919d2f568fce17d17aad9e",
+          "message": "chore: benches",
+          "timestamp": "2026-09-11T12:44:16+02:00",
+          "tree_id": "0547f6994d6c82811c1648d302b77250cb93ce6d",
+          "url": "https://github.com/dasantos/ferx/commit/535ddeb64f4f124435919d2f568fce17d17aad9e"
+        },
+        "date": 1789123908446,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "next_no_receivers",
+            "value": 12,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "next_with_a_receiver",
+            "value": 19,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "next_after_termination",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
